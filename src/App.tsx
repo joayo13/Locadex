723ec8aa44dbcf2eeba21c5ffbe43a8135281b01
Jordan Ayotte <jordanayotte13@outlook.com>
2024-07-21 15:14:06 -0400

    added html input for photo

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Ventura
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+<label htmlFor="picture">Take a picture using back facing camera:</label>
+
+<input type="file" id="picture" name="picture" accept="image/*" capture="environment" />
+
       </header>
     </div>
   );
