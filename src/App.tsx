009fb300c0d63e0dbf48d7def323bd51c7bd4e59
Jordan Ayotte <jordanayotte13@outlook.com>
2024-08-02 23:42:41 -0400

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ImageUploader from './components/ImageUploader';
 import NewLocationGenerator from './components/NewLocationGenerator';
+import {app} from'../src/services/firebase'
+//TODO: Implement this haversine equation for checking distance between lat longs 
+// function distance(lat1, lon1, lat2, lon2) {
+//   const r = 6371; // km
+//   const p = Math.PI / 180;
+
+//   const a = 0.5 - Math.cos((lat2 - lat1) * p) / 2
+//                 + Math.cos(lat1 * p) * Math.cos(lat2 * p) *
+//                   (1 - Math.cos((lon2 - lon1) * p)) / 2;
+
+//   return 2 * r * Math.asin(Math.sqrt(a));
+// }
 const App: React.FC = () => {
+  useEffect(() => {
+    console.log(app)
+  },[])
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/services/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "ventura-e1865.firebaseapp.com",
+  projectId: "ventura-e1865",
+  storageBucket: "ventura-e1865.appspot.com",
+  messagingSenderId: "658474063946",
+  appId: "1:658474063946:web:16a47ff8f722a94f5e6eb3",
+  measurementId: "G-JRK3JF655J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export {app, analytics}
