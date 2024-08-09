533a719a63752ffea3cacfc7fdf99035dbbea069
Jordan Ayotte <jordanayotte13@outlook.com>
2024-08-09 15:03:44 -0400

    Revert "added fixed version, should work on mobile with google auth"
    This reverts commit ca068971dcec62eb6a559c9fbf8e100343e56c46.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import ImageUploader from './components/ImageUploader';
 import NewLocationGenerator from './components/NewLocationGenerator';
-import {app, auth, googleSignIn} from'./services/firebase'
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import {app} from'./services/firebase'
 //TODO: Implement this haversine equation for checking distance between lat longs 
 // function distance(lat1, lon1, lat2, lon2) {
 //   const r = 6371; // km
@@ -25,7 +24,6 @@ const App: React.FC = () => {
         <p>
           Ventura
         </p>
-        <div id="firebaseui-auth-container"></div>
 <ImageUploader/>
 <NewLocationGenerator/>
 <button onClick={() => signInWithGoogle()}>Google sign in</button>
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -20,54 +20,29 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+signInWithRedirect(auth, provider);
+getRedirectResult(auth)
+  .then((result) => {
+    
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
 
-const googleSignIn = async () => {
-  try {
-    await signInWithRedirect(auth, provider);
-  }
-  catch(error) {
-    console.log(error.code, error.message)
-  }
-  
-}
-// const debugGoogleRedirectResult = async () => {
-//   try {
-//     const result = await getRedirectResult(auth)
-//     if(result) {
-//       const credential = GoogleAuthProvider.credentialFromResult(result);
-//       const token = credential.accessToken;
-//       // The signed-in user info.
-//       const user = result.user;
-//       console.log(user, token)
-//       // IdP data available using getAdditionalUserInfo(result)
-//       // ...
-//     }
-//     else {
-//       console.log('what')
-//     }
-//   }
-//   catch(error) {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     // The AuthCredential type that was used.
-//     const credential = GoogleAuthProvider.credentialFromError(error);
-//     console.log(errorCode, errorMessage, credential);
-//   }
-// }
-// onAuthStateChanged(auth, async (user) => {
-//   if (user) {
-//       const result = await getRedirectResult(auth);
-//       if (result) {
-//         const credential = GoogleAuthProvider.credentialFromResult(result);
-//         const token = credential.accessToken;
-//         // The signed-in user info.
-//         const user = result.user;
-//         console.log(user, token)
-//         // IdP data available using getAdditionalUserInfo(result)
-//         // ...
-//       }
-//   }
-// });
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user, token)
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log(errorCode, errorMessage, credential);
+    // ...
+  });
 
 export {app, googleSignIn, auth}
