269ca7c4637324d5871961d7749c8fbaf01238a8
Jordan Ayotte <jordanayotte13@outlook.com>
2024-09-05 17:25:38 -0400

    mobile menu slides out now

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -21,8 +21,18 @@ function MainNav({user, signInFormVisible, setSignInFormVisible}: MainNavProps) 
 		toggleMenu();
 	}
   return (
-    <nav className='bg-neutral-900 text-neutral-200 flex justify-between items-center px-2 h-16'><p className='text-xl text-blue-400'>Ventura</p>
-    <div onClick={handleOverlayClick} className={`overlay ${menuOpen ? 'animate-layout-open' : 'animate-layout-close'}`}></div>
+    <nav className='bg-neutral-900 shadow-lg text-neutral-200 flex justify-between items-center px-2 h-16'><button className='text-xl text-blue-400'>Ventura</button>
+    <div onClick={handleOverlayClick} className={`overlay ${menuOpen ? 'animate-overlay-open' : 'animate-overlay-close'}`}></div>
+    <nav aria-label='mobile-menu' className={`mobile-menu bg-blue-950 text-neutral-200 p-4 ${menuOpen ? 'animate-mobile-menu-open' : 'animate-mobile-menu-close'}`}>
+      <ul className='flex flex-col items-start gap-8'>
+        <button className='underline'>My Adventure</button>
+        <button className='underline'>Location Index</button>
+        <span className='h-px bg-neutral-200 w-full'></span>
+        {user ? <p>Logged in as {user.email}</p> : null}
+        {user ? <button onClick={() => signOutUser(auth)} className='underline'>Sign Out</button> : <button onClick={() =>{ setSignInFormVisible(true); setMenuOpen(false)}} className='underline'>Sign In</button>}
+
+      </ul>
+    </nav>
     <button
 					className={`hamburger hamburger--collapse ${menuOpen ? 'is-active' : ''}`}
 					onClick={toggleMenu}
