ddc664664253fc979197a5771351befa7f8c5a71
Jordan Ayotte <jordanayotte13@outlook.com>
2024-09-05 14:29:45 -0400

    added navbar, almost figured out forward/backward animation in react. svelte is so much nicer to use jeez

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { auth, signOutUser } from '../services/firebase'
 import { User } from 'firebase/auth';
 interface MainNavProps {
@@ -8,15 +8,32 @@ interface MainNavProps {
 }
 
 function MainNav({user, signInFormVisible, setSignInFormVisible}: MainNavProps) {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [overlay, setOverlay] = useState(false)
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+    if(overlay === false) {
+      setOverlay(true)
+    }
+    document.body.style.overflow = menuOpen ? 'hidden' : 'auto';
+  }
+  function handleOverlayClick() {
+		toggleMenu();
+	}
   return (
     <nav className='bg-neutral-900 text-neutral-200 flex justify-between items-center px-2 h-16'><p className='text-xl text-blue-400'>Ventura</p>
-    {!user ? (
-        <button className='bg-blue-800 py-2 px-4 rounded'
-            onClick={() => setSignInFormVisible(!signInFormVisible)}
-        >
-            Sign In
-        </button>
-    ) : <button onClick={() => signOutUser(auth)}>Sign Out</button>}</nav>
+    {overlay ? <div onClick={handleOverlayClick} className={`overlay ${menuOpen ? 'animate-layout-open' : 'animate-layout-close'}`} onAnimationEnd={() => {if(!menuOpen) setOverlay(false); console.log('okay')}}></div> : null}
+    <button
+					className={`hamburger hamburger--collapse ${menuOpen ? 'is-active' : ''}`}
+					onClick={toggleMenu}
+					aria-expanded={menuOpen}
+					aria-controls="mobile-menu"
+				>
+					<span className="hamburger-box">
+						<span className="hamburger-inner"></span>
+					</span>
+				</button>
+    </nav>
   )
 }
 
