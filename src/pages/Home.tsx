2b5a56751f5127f40ed32d535c1e45e02de1c3b0
Jordan Ayotte <jordanayotte13@outlook.com>
2024-09-16 13:45:30 -0400

    added current user dashbaord

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useAuth } from '../contexts/AuthContext';
 import LoadingScreen from './LoadingScreen';
+import { User } from 'firebase/auth';
 
 function Home() {
     const [isMobileDevice, setIsMobileDevice] = useState(false);
@@ -18,22 +19,18 @@ function Home() {
     </div>
       )
     }
-    const UserPage = () => {
+    const UserPage = (currentUser: User) => {
       return (
         <div className='bg-stone-950 min-h-screen text-stone-200 px-4'>
         {isMobileDevice ? <div></div> : null}
-        <h1 className='text-6xl py-4 playfair text-orange-400'>Your next adventure awaits user.</h1>
-        <h2 className='text-3xl pb-4 playfair text-stone-200'>Introducing Locadex</h2>
-        <p>The Locadex (Location-Index) is an all-in-one travel companion that uses GPS and Google Maps to give you fun things to do near you.</p>
-        <p>Track your progress over time and keep on hunting down new locations. It's as simple as that.</p>
-        <p>The Locadex features a built in map, built in GPS, locator, and an index that contains all the places you've been.</p>
-        <p>Open up the app, and let it be your guide. It's an answer to the question "What should I do today?"</p>
+        <h1 className='text-6xl py-4 playfair text-orange-400'>Dashboard</h1>
+        <h2 className='pb-4 playfair text-stone-200'>{ currentUser.email }</h2>
     </div>
       )
     }
     function returnUserOrGuestPage() {
       if(currentUser) {
-        return UserPage()
+        return UserPage(currentUser)
       }
       else {
         return GuestPage()
